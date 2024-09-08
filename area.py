52f72483066512eaa39b1d9a2bda5587e85bc161
Ursmaax <sativadau@gmail.com>
2024-09-08 09:40:38 +0530

    Initial commit

---
+++ area.py
@@ -0,0 +1,8 @@
+from operator import length_hint
+
+width = input('enter width here :')
+length = input('enter length here : ')
+width = int(width)
+length = int(length)
+print(f"area of the rectangle is {width * length} !")
+
---
+++ madlibs.py
@@ -0,0 +1,23 @@
+#Madlibs game
+
+print("Let's play a small game, dear one ...!")
+print("")
+print("Just answer the following questions...!")
+print("")
+
+your_name = input("Enter your Name (e.g., John)                       :")
+fav_food1 = input("Enter your favorite food (e.g., Pizza)             :")
+fav_food2 = input("Enter another favorite food (e.g., Ice Cream)      :")
+animal_name = input("Enter an Animal name (e.g., Lion)                :")
+fast = input("Enter a word to describe speed (e.g., Fast)             :")
+day_description = input("Enter a word to describe the day (e.g., Fun) :")
+print("                         \nHere is your small  funny story !!                  ")
+print("")
+print(f"I went on a picnic with {your_name}.")
+print(f"We brought {fav_food1} to eat.")
+print(f"Suddenly, a {animal_name} took our {fav_food1} and ran away! We tried to get it back, but it was too {fast}.")
+print(f"So, we sat down and ate {fav_food2} instead. It was a {day_description} day!")
+print("")
+
+
+print("                    Heartfelt thanks to you for trying my game!!                     ")
