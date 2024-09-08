58a8eb98dfda5e98a22cd1e0a0cdb6d42747afa8
Ursmaax <167686833+Ursmaax@users.noreply.github.com>
2024-09-08 09:42:28 +0530

    Delete area.py

--- area.py
+++
@@ -1,8 +0,0 @@
-from operator import length_hint
-
-width = input('enter width here :')
-length = input('enter length here : ')
-width = int(width)
-length = int(length)
-print(f"area of the rectangle is {width * length} !")
-
